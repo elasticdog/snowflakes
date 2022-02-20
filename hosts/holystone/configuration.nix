a5630ba9873d800f479025e67ea592e912ebf6a2
Aaron Bull Schaefer <aaron@elasticdog.com>
2022-02-20 11:07:12 -0800

    Install brillo for managing the screen backlight
    I'll still need to get the keybindings working, but this is a system
    level control using graphic driver exposed under `/sys/class/backlight`.
    This has a very similar interface to `light`, but is actively maintained
    and supports additional features like smooth changes, easily managing
    multiple devices, etc.
    
    See:
    - https://gitlab.com/cameronnemo/brillo
    - https://wiki.archlinux.org/title/backlight

--- hosts/holystone/configuration.nix
+++ hosts/holystone/configuration.nix
@@ -27,6 +27,9 @@
     };
   };
 
+  # screen backlight control
+  hardware.brillo.enable = true;
+
   home-manager.useGlobalPkgs = true;
   home-manager.useUserPackages = true;
   home-manager.users.abs = import ../../home/abs/holystone.nix;
