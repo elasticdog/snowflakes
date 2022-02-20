a605aa7c2b15841658fc29c10d5cca9c97e0e4d7
Aaron Bull Schaefer <aaron@elasticdog.com>
2022-02-20 15:36:50 -0800

    Autostart sway after user login
    I'm not sure if this will work with fish, but we'll find out!

--- modules/wayland.nix
+++ modules/wayland.nix
@@ -1,6 +1,13 @@
 {
   programs.sway.enable = true;
 
+  # autostart sway after user login
+  environment.loginShellInit = ''
+    if [ -z $DISPLAY ] && [ "$(tty)" = "/dev/tty1" ]; then
+      exec sway
+    fi
+  '';
+
   # allow for screen sharing
   xdg.portal.wlr.enable = true;
 }
