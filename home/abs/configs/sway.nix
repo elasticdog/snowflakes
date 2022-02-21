8c06ce4646f9e2d34cfd99c920b293c677d73068
Aaron Bull Schaefer <aaron@elasticdog.com>
2022-02-20 21:20:04 -0800

    Handle brightness keys with sway keybindings
    This works more reliably...I think there is another means to have these
    same things in place in the console, but that's less important. I've
    also tweaked the modifier key to use `Super` since Kakoune and other
    things expect `Alt` to be usable.

--- home/abs/configs/sway.nix
+++ home/abs/configs/sway.nix
@@ -1,13 +1,27 @@
+{ config, lib, ... }:
+
 {
   wayland.windowManager.sway = {
     enable = true;
 
-    config.input = {
-      * = {
-        xkb_options = "altwin:prtsc_rwin,ctrl:nocaps,shift:both_capslock";
+    config = {
+      input = {
+        "*" = { xkb_options = "altwin:prtsc_rwin,ctrl:nocaps,shift:both_capslock"; };
       };
-    };
 
-    config.terminal = "kitty";
+      keybindings =
+        let
+          modifier = config.wayland.windowManager.sway.config.modifier;
+        in
+        lib.mkOptionDefault {
+          "XF86MonBrightnessDown" = "exec /run/current-system/sw/bin/brillo -q -U 8";
+          "XF86MonBrightnessUp" = "exec /run/current-system/sw/bin/brillo -q -A 10";
+          "Shift+XF86MonBrightnessDown" = "exec /run/current-system/sw/bin/brillo -u 100000 -q -S 0";
+          "Shift+XF86MonBrightnessUp" = "exec /run/current-system/sw/bin/brillo -u 100000 -q -S 100";
+        };
+
+      modifier = "Mod4"; # Super
+      terminal = "kitty";
+    };
   };
 }
--- hosts/holystone/hotkey-functions.nix
+++ hosts/holystone/hotkey-functions.nix
@@ -1,45 +1,4 @@
 {
   # screen backlight control
   hardware.brillo.enable = true;
-
-  # system-level keyboard shortcut daemon
-  #
-  #     $ sudo actkbd --noexec --showkey --device <device>
-  #
-  # /dev/input/event0 = "AT Translated Set 2 keyboard"
-  #
-  #   keys  56 = Alt_L
-  #   keys 100 = Alt_R
-  #   keys 113 = XF86AudioMute
-  #   keys 114 = XF86AudioLowerVolume
-  #   keys 115 = XF86AudioRaiseVolume
-  #
-  # /dev/input/event5 = "Video Bus"
-  #
-  #   keys 224 = XF86MonBrightnessDown
-  #   keys 225 = XF86MonBrightnessUp
-  #
-  # /dev/input/event10 "ThinkPad Extra Buttons"
-  #
-  #   keys 190 = XF86AudioMicMute
-  #   keys 227 = XF86Display
-  #   keys 238 = XF86WLAN
-  #   keys 444 = XF86NotificationCenter
-  #   keys 445 = XF86PickupPhone
-  #   keys 446 = XF86HangupPhone
-  #   keys 156 = XF86Favorites
-  services.actkbd = {
-    enable = true;
-    bindings = [
-      # XF86MonBrightnessDown
-      { keys = [ 224 ]; events = [ "key" "rep" ]; command = "/run/current-system/sw/bin/brillo -q -U 8 &"; }
-      { keys = [ 224 56 ]; events = [ "key" ]; command = "/run/current-system/sw/bin/brillo -u 1500000 -q -S 0 &"; }
-      { keys = [ 224 100 ]; events = [ "key" ]; command = "/run/current-system/sw/bin/brillo -u 1500000 -q -S 0 &"; }
-
-      # XF86MonBrightnessUp
-      { keys = [ 225 ]; events = [ "key" "rep" ]; command = "/run/current-system/sw/bin/brillo -q -A 10 &"; }
-      { keys = [ 225 56 ]; events = [ "key" ]; command = "/run/current-system/sw/bin/brillo -u 1500000 -q -S 100 &"; }
-      { keys = [ 225 100 ]; events = [ "key" ]; command = "/run/current-system/sw/bin/brillo -u 1500000 -q -S 100 &"; }
-    ];
-  };
 }
