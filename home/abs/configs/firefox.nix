091efce37d8b2aa18121c1968e738bc318a01558
Aaron Bull Schaefer <aaron@elasticdog.com>
2022-02-21 14:58:43 -0800

    Generate XDG Desktop entries for all Firefox profiles

--- home/abs/configs/firefox.nix
+++ home/abs/configs/firefox.nix
@@ -20,4 +20,32 @@
       };
     };
   };
+
+  xdg.desktopEntries =
+    let
+      makeDesktopEntry = profile: {
+        name = "Firefox (${profile})";
+        genericName = "Web Browser";
+        categories = [ "Network" "WebBrowser" ];
+        icon = "firefox";
+        exec = "firefox --profile ${profile} %U";
+        startupNotify = true;
+        terminal = false;
+        mimeType = [
+          "application/vnd.mozilla.xul+xml"
+          "application/xhtml+xml"
+          "application/x-xpinstall"
+          "text/html"
+          "text/xml"
+          "x-scheme-handler/ftp"
+          "x-scheme-handler/http"
+          "x-scheme-handler/https"
+        ];
+      };
+    in
+    {
+      firefox = (makeDesktopEntry "work");
+      firefox-home = (makeDesktopEntry "home");
+      firefox-compatibility = (makeDesktopEntry "compatibility");
+    };
 }
