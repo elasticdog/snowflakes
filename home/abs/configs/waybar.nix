464c053fc2cabf3459d337551e8fcb8b8ab16cfd
Aaron Bull Schaefer <aaron@elasticdog.com>
2022-02-21 16:06:02 -0800

    Use the documented minimal config for Waybar
    I'll tweak this soon, and the default configuration file can be found
    under the path `/etc/profiles/per-user/abs/etc/xdg/waybar/config`.
    Note that none of the icons are currently working due to font
    incompatibility.

--- home/abs/configs/waybar.nix
+++ home/abs/configs/waybar.nix
@@ -2,5 +2,29 @@
   programs.waybar = {
     enable = true;
     systemd.enable = true;
+
+    settings = {
+      mainBar = {
+        layer = "top";
+        position = "top";
+
+        modules-left = [ "sway/workspaces" "sway/mode" ];
+        modules-center = [ "sway/window" ];
+        modules-right = [ "battery" "clock" ];
+
+        "sway/window" = {
+          max-length = 50;
+        };
+
+        battery = {
+          format = "{capacity}% {icon}";
+          format-icons = [ "" "" "" "" "" ];
+        };
+
+        clock = {
+          format-alt = "{:%a, %d. %b  %H:%M}";
+        };
+      };
+    };
   };
 }
