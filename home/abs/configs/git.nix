1a227cc63f692d16adffff4c7890f593476db5c0
Aaron Bull Schaefer <aaron@elasticdog.com>
2022-02-24 21:04:39 -0800

    Add initial git configuration files
    This doesn't replicate everything I had, but a lot of it was unused cruft, so
    I'll just add to it over time.

--- home/abs/configs/git.nix
+++ home/abs/configs/git.nix
@@ -1,7 +1,34 @@
 {
   programs.git = {
     enable = true;
-    userName = "Aaron Bull Schaefer";
     userEmail = "aaron@elasticdog.com";
+    userName = "Aaron Bull Schaefer";
+
+    aliases = {
+    };
+
+    # https://github.com/dandavison/delta
+    delta.enable = true;
+
+    extraConfig = {
+      commit.verbose = true;
+      init.defaultBranch = "main";
+      push.default = "current";
+      remote.pushdefault = "origin";
+      user.useConfigOnly = true;
+    };
+
+    ignores = [
+      ".envrc"
+    ];
+
+    includes = [
+      {
+        condition = "gitdir:~/work/";
+        contents = {
+          user.email = "abs@elastic.co";
+        };
+      }
+    ];
   };
 }
