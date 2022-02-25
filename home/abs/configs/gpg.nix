6d76ed858cc280ed39b449f94c239b4a7595a53f
Aaron Bull Schaefer <aaron@elasticdog.com>
2022-02-24 22:11:21 -0800

    Bump up the TTLs on the SSH key caching
    This is already an improvement over my previous workflows where it would be
    cached forever, but the default 30 minutes is too low. This will allow me to
    extend usage for a day's worth of waking hours, but if I don't actually use it,
    it will expire in two hours.

--- home/abs/configs/gpg.nix
+++ home/abs/configs/gpg.nix
@@ -4,5 +4,9 @@
   services.gpg-agent = {
     enable = true;
     enableSshSupport = true;
+    defaultCacheTtlSsh = 7200; # 2h
+    extraConfig = ''
+      max-cache-ttl-ssh 57600
+    '';
   };
 }
